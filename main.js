ed449b05bafe0931c461059d03ee8c5d00f1c036
Vitalii Salai <vitalii.v.salai@gmail.com>
2020-11-15 17:45:58 +0200

    fix empty task add

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ class Form {
         title.classList.add('header')
         title.textContent = text
 
-        return title   
+        return title
     }
 
     renderTaskForm = (text) => {
@@ -24,7 +24,7 @@ class Form {
         this.taskInput.classList.add('task-input')
         this.taskInput.setAttribute('type', 'text')
 
-        this.createButton.classList.add("create-button","far","fa-plus-square")
+        this.createButton.classList.add("create-button", "far", "fa-plus-square")
 
         this.tasksList.classList.add('tasks-list')
 
@@ -33,11 +33,8 @@ class Form {
         this.container.append(this.tasksList)
 
         document.body.append(title)
-        document.body.append(this.container)       
+        document.body.append(this.container)
     }
-
-
-
 }
 
 class Task {
@@ -49,15 +46,15 @@ class Task {
         const taskContainer = document.createElement('li')
         const task = document.createElement('p')
         const taskInput = document.querySelector('.task-input')
-    
+
         const completeButton = document.createElement('input')
         const editButton = document.createElement('button')
         const deleteButton = document.createElement('button')
 
         completeButton.setAttribute('type', 'checkbox')
         completeButton.classList.add("comp-button")
-        editButton.classList.add("edit-button","far","fa-edit")
-        deleteButton.classList.add("delete-button","far","fa-trash-alt")
+        editButton.classList.add("edit-button", "far", "fa-edit")
+        deleteButton.classList.add("delete-button", "far", "fa-trash-alt")
         taskContainer.classList.add('task-container')
         task.classList.add('task')
         task.textContent = taskInput.value
@@ -73,14 +70,14 @@ class Task {
     static getChildren = (elem) => {
         const children = []
         let child = elem.parentNode.firstChild
-    
+
         while (child) {
             if (child.nodeType === 1 && child !== elem) {
                 children.push(child)
             }
             child = child.nextSibling
         }
-    
+
         return children
     }
 
@@ -88,8 +85,8 @@ class Task {
         const taskContainer = Task.renderTask()
         const tasksList = document.querySelector('.tasks-list')
         const taskInput = document.querySelector('.task-input')
-        
-        if (taskInput.value != '') {
+
+        if (taskInput.value.trim() != '') {
             tasksList.append(taskContainer)
             taskInput.value = ''
         }
@@ -98,13 +95,13 @@ class Task {
     completeTask = (event) => {
         const completeButton = event.target
         const children = Task.getChildren(completeButton)
-    
+
         if (completeButton.classList.contains('comp-button') && completeButton.checked == true) {
             children[0].classList.add("completed")
             children[1].classList.add("disabled")
             children[1].setAttribute('disabled', '')
         }
-        else if (completeButton.classList.contains('comp-button') && completeButton.checked == false){
+        else if (completeButton.classList.contains('comp-button') && completeButton.checked == false) {
             children[0].classList.remove("completed")
             children[1].classList.remove("disabled")
             children[1].removeAttribute('disabled', '')
@@ -114,7 +111,7 @@ class Task {
 
     editTask = (event) => {
         const editButton = event.target
-    
+
         if (editButton.classList.contains('edit-button')) {
             const children = Task.getChildren(editButton)
             const task = children[1]
@@ -126,8 +123,8 @@ class Task {
             editTaskInput.value = task.textContent
             task.replaceWith(editTaskInput)
             task.remove()
-            editButton.classList.remove("edit-button","fa-edit")
-            editButton.classList.add("save-button","fa-save")
+            editButton.classList.remove("edit-button", "fa-edit")
+            editButton.classList.add("save-button", "fa-save")
             deleteButtton.classList.add("disabled")
             completeButton.classList.add("disabled")
             deleteButtton.setAttribute('disabled', '')
@@ -141,24 +138,24 @@ class Task {
     saveTask = (event) => {
         const saveButton = event.target
         const task = document.createElement('p')
-    
+
         if (saveButton.classList.contains('save-button')) {
             const children = Task.getChildren(saveButton)
             const editTaskInput = children[1]
             const deleteButtton = children[2]
             const completeButton = children[0]
-    
+
             task.textContent = editTaskInput.value
             editTaskInput.replaceWith(task)
             task.classList.add('task')
             editTaskInput.remove()
-            saveButton.classList.add("edit-button","fa-edit")
-            saveButton.classList.remove("save-button","fa-save")
+            saveButton.classList.add("edit-button", "fa-edit")
+            saveButton.classList.remove("save-button", "fa-save")
             deleteButtton.classList.remove("disabled")
             completeButton.classList.remove("disabled")
             deleteButtton.removeAttribute('disabled', '')
             completeButton.removeAttribute('disabled', '')
-    
+
             tasksList.removeEventListener('click', this.saveTask)
             tasksList.addEventListener('click', this.editTask)
         }
@@ -189,8 +186,3 @@ createButton.addEventListener('click', task.addTask)
 tasksList.addEventListener('click', task.completeTask)
 tasksList.addEventListener('click', task.editTask)
 tasksList.addEventListener('click', task.deleteTask)
-
-
-
-
-
