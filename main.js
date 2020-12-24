d480e4e437b6651f6f2535bd31abe3f60232802b
Vitalii Salai <vitalii.v.salai@gmail.com>
2020-12-24 14:39:39 +0200

    refactor

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ class EventEmitter {
         this.events = {}
     }
 
-    on(eventName, callback) {
+    on = (eventName, callback) => {
         if (!this.events[eventName]) {
             this.events[eventName] = []
         }
@@ -15,7 +15,7 @@ class EventEmitter {
         }
     }
 
-    emit(eventName, data) {
+    emit = (eventName, data) => {
         const event = this.events[eventName]
         if (event) {
             event.forEach(callback => { callback.call(null, data) })
@@ -23,33 +23,31 @@ class EventEmitter {
     }
 }
 
-class Form {
-    constructor(name, tag, type) {
-        this.name = name
-        this.title = document.createElement(tag)
-        this.container = document.createElement('div')
-        this.tasksList = document.createElement('ul')
-        this.taskInput = document.createElement('input')
-        this.createButton = document.createElement('button')
-        this.editTaskInput = document.createElement('input')
+class Render {
+    constructor() { }
 
-        this.type = type
+    setAttributes(elem, attrs) {
+        for (const key in attrs) {
+            elem.setAttribute(key, attrs[key])
+        }
     }
 
-    renderForm() {
-        this.taskInput.classList.add('task-input')
-        this.taskInput.setAttribute('type', 'text')
-        this.createButton.classList.add('create-button', 'far', 'fa-plus-square')
-        this.container.append(this.taskInput)
-        this.container.append(this.createButton)
-        this.title.textContent = this.name
-        this.title.classList.add('header')
-        this.container.classList.add('container')
-        this.container.append(this.tasksList)
-        this.tasksList.classList.add(this.type)
+    createElem(props) {
+        const elem = document.createElement(props.tag)
+        if (props.classNames) {
+            elem.classList.add(...props.classNames)
+        }
+        if (props.attributes) {
+            this.setAttributes(elem, props.attributes)
+        }
+        if (props.textContent) {
+            elem.textContent = props.textContent
+        }
+        if (props.value) {
+            elem.value = props.value
+        }
 
-        document.body.append(this.title)
-        document.body.append(this.container)
+        return elem
     }
 }
 
@@ -63,68 +61,143 @@ class Task {
     }
 }
 
-class Render {
-    constructor(tasksList) {
-        this.tasksList = tasksList
+class Form extends Render {
+    constructor(tag, classNames, textContent, listType, root) {
+        super()
+
+        this.title = {
+            tag: tag,
+            classNames: [classNames],
+            textContent: textContent,
+        }
+
+        this.listType = listType
+        this.root = root
+    }
+
+    renderForm = () => {
+        const title = super.createElem(this.title)
+        const tasksContainer = super.createElem({
+            tag: 'div',
+            classNames: ['container']
+        })
+        const container = super.createElem({
+            tag: 'div'
+        })
+        const tasksList = super.createElem({
+            tag: 'ul',
+            classNames: [this.listType]
+        })
+        const taskInput = super.createElem({
+            tag: 'input',
+            classNames: ['task-input']
+        })
+        const createButton = super.createElem({
+            tag: 'button',
+            classNames: ['create-button', 'far', 'fa-plus-square']
+        })
+
+        tasksContainer.append(taskInput)
+        tasksContainer.append(createButton)
+        tasksContainer.append(tasksList)
+
+        container.append(title)
+        container.append(tasksContainer)
+
+
+        return container
+    }
+
+    render = () => {
+        while (this.root.firstChild)
+            this.root.removeChild(this.root.firstChild)
+        const form = this.renderForm()
+        this.root.append(form)
+    }
+}
+
+class RenderTask extends Render {
+    constructor() {
+        super()
     }
 
     createTask = (task) => {
 
         if (task.isDeleted != true) {
-            const taskContainer = document.createElement('li')
-            const taskLabel = document.createElement('p')
-            const completeButton = document.createElement('input')
-            const editButton = document.createElement('button')
-            const deleteButton = document.createElement('button')
-            const editTaskInput = document.createElement('input')
 
-            editTaskInput.setAttribute('type', 'text')
-            editTaskInput.classList.add('edit-view')
-            completeButton.setAttribute('type', 'checkbox')
-            completeButton.classList.add('comp-button')
-            editButton.classList.add('far')
-            deleteButton.classList.add('delete-button', 'far', 'fa-trash-alt')
-            taskContainer.classList.add('task-container')
-            taskContainer.setAttribute('data-id', task.taskId)
-            taskLabel.classList.add('task')
-
-            if (task.isCompleted == true) {
-                completeButton.setAttribute('checked', '')
-                taskLabel.classList.add('completed')
-                editButton.classList.add('disabled')
+            const complete = {
+                tag: 'input',
+                classNames: ['comp-button'],
+                attributes: { 'type': 'checkbox', }
             }
+            const container = {
+                tag: 'li',
+                classNames: ['task-container'],
+                attributes: { 'data-id': task.taskId }
+            }
+            const label = {
+                tag: 'p',
+                classNames: ['task'],
+                textContent: task.taskLabel
+            }
+            const edit = {
+                tag: 'button',
+                classNames: ['far']
+            }
+            const del = {
+                tag: 'button',
+                classNames: ['delete-button', 'far', 'fa-trash-alt'],
 
-            taskContainer.append(completeButton)
+            }
+            if (task.isCompleted == true) {
+                complete.attributes['checked'] = ''
+                label.classNames.push('completed')
+                edit.classNames.push('disabled')
+            }
 
             if (task.editView == true) {
-                editTaskInput.value = task.taskLabel
-                editButton.classList.add('save-button', 'fa-save')
-                deleteButton.classList.add('disabled')
-                completeButton.classList.add('disabled')
-                deleteButton.setAttribute('disabled', '')
-                completeButton.setAttribute('disabled', '')
-                taskContainer.append(editTaskInput)
+                label.tag = 'input'
+                label.classNames = ['edit-view']
+                label.attributes = { 'type': 'text' }
+                label.value = task.taskLabel
+
+                edit.classNames.push('save-button', 'fa-save')
+
+                del.classNames.push('disabled')
+                del.attributes = { 'disabled': '' }
+
+                complete.classNames.push('disabled')
+                complete.attributes['disabled'] = ''
             } else if (task.editView == false) {
-                taskLabel.textContent = task.taskLabel
-                editButton.classList.add('edit-button', 'fa-edit')
-                taskContainer.append(taskLabel)
+                edit.classNames.push('edit-button', 'fa-edit')
             }
 
+            const taskContainer = super.createElem(container)
+            const completeButton = super.createElem(complete)
+            const taskLabel = super.createElem(label)
+            const editButton = super.createElem(edit)
+            const deleteButton = super.createElem(del)
+
+            taskContainer.append(completeButton)
+            taskContainer.append(taskLabel)
             taskContainer.append(editButton)
             taskContainer.append(deleteButton)
 
-            this.tasksList.append(taskContainer)
+            return taskContainer
         }
 
         else return
     }
 
-    render = (tasks) => {
-        while (this.tasksList.firstChild) {
-            this.tasksList.removeChild(this.tasksList.firstChild)
+    render = (root, tasks) => {
+        while (root.firstChild) {
+            root.removeChild(root.firstChild)
         }
         for (const task of tasks) {
-            this.createTask(task)
+            const item = this.createTask(task)
+            if (item != undefined) {
+                root.append(item)
+            }
         }
     }
 }
@@ -177,7 +250,7 @@ class Store {
             this.tasks[taskId].editView = false
             this.tasks[taskId].taskLabel = value
         }
-        
+
         return this.tasks
     }
 
@@ -188,26 +261,28 @@ class Store {
     }
 }
 
-const emitter = new EventEmitter()
-const tasksForm = new Form('Tasks', 'h1', 'tasks-list')
+const root = document.querySelector('.root')
 
-tasksForm.renderForm()
+const emitter = new EventEmitter()
+const tasksForm = new Form('h1', 'header', 'Tasks', 'tasks-list', root)
+
+document.addEventListener('readystatechange', tasksForm.render())
 
 const createButton = document.querySelector('.create-button')
 const tasksList = document.querySelector('.tasks-list')
 const tasksInput = document.querySelector('.task-input')
 
 const store = new Store()
-const render = new Render(tasksList)
+const render = new RenderTask()
 
 emitter.on('list-changed', data => {
-    render.render(data.tasks)
+    render.render(data.root, data.tasks)
 })
 
 createButton.addEventListener('click', () => {
     const tasks = store.addTask(tasksInput)
     if (tasks != undefined) {
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 
     tasksInput.value = ''
@@ -217,7 +292,7 @@ tasksInput.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
         const tasks = store.addTask(tasksInput)
         if (tasks != undefined) {
-            emitter.emit('list-changed', { tasks: tasks })
+            emitter.emit('list-changed', { tasks: tasks, root: tasksList })
         }
 
         tasksInput.value = ''
@@ -228,7 +303,7 @@ tasksList.addEventListener('click', (event) => {
     if (event.target.classList.contains('delete-button')) {
         const taskId = +event.target.closest('li').dataset.id
         const tasks = store.deleteTask(taskId)
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 })
 
@@ -236,7 +311,7 @@ tasksList.addEventListener('click', (event) => {
     if (event.target.classList.contains('comp-button')) {
         const taskId = +event.target.closest('li').dataset.id
         const tasks = store.completeTask(taskId)
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 })
 
@@ -244,12 +319,12 @@ tasksList.addEventListener('click', (event) => {
     if (event.target.classList.contains('edit-button')) {
         const taskId = +event.target.closest('li').dataset.id
         const tasks = store.editTask(taskId)
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     } else if (event.target.classList.contains('save-button')) {
         const taskId = +event.target.closest('li').dataset.id
         const value = event.target.previousSibling.value
         const tasks = store.editTask(taskId, value)
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 })
 
@@ -259,13 +334,13 @@ tasksList.addEventListener('keyup', (event) => {
         const id = +task.closest('li').dataset.id
         const value = task.value
         const tasks = store.editTask(id, value)
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 })
 
 document.addEventListener('keyup', (event) => {
     if (event.key === 'Escape') {
         const tasks = store.cancelEdit()
-        emitter.emit('list-changed', { tasks: tasks })
+        emitter.emit('list-changed', { tasks: tasks, root: tasksList })
     }
 })
