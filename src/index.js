3f92154966b690cf571b26f7ba112d7f31aa4b23
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-04-18 21:57:59 +0300

    fix

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import 'regenerator-runtime/runtime';
-import './scss/style.scss';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-var root = document.querySelector('.root');
-ReactDOM.render(_jsx(App, {}, void 0), root);
