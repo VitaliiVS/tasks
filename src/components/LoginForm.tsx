bca7803b962641faaf49d2d6684684b9a57a479e
Vitalii Salai <vitalii.v.salai@gmail.com>
2021-05-17 16:18:47 +0300

    refactor LoginForm to functional

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Session } from './session'
 import { debounce } from '../common/helpers'
 import { loginUrl, registerUrl } from '../common/config'
@@ -9,124 +9,91 @@ interface LoginProps {
   onTokenChange: (token: string) => void
 }
 
-interface LoginState {
-  username: string
-  password: string
-}
+const LoginForm = (props: LoginProps): JSX.Element => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-class LoginForm extends React.Component<LoginProps, LoginState> {
-  loginUrl: string
-  registerUrl: string
-  handleLoginDebounced: () => void
-  handleRegisterDebounced: () => void
-
-  constructor(props: LoginProps) {
-    super(props)
-
-    this.loginUrl = loginUrl
-    this.registerUrl = registerUrl
-
-    this.handleLoginDebounced = debounce(this.handleLogin, 200)
-    this.handleRegisterDebounced = debounce(this.handleRegister, 200)
-
-    this.state = {
-      username: '',
-      password: ''
-    }
-  }
-
-  handleLogin = async (): Promise<void> => {
-    const { onTokenChange } = this.props
-    const { username, password } = this.state
+  const handleLogin = async (): Promise<void> => {
+    const { onTokenChange } = props
 
     try {
-      const login = await session.login(this.loginUrl, username, password)
+      const login = await session.login(loginUrl, username, password)
       onTokenChange(login)
     } catch (e) {
       alert(e.message)
     }
   }
 
-  handleRegister = async (): Promise<void> => {
-    const { onTokenChange } = this.props
-    const { username, password } = this.state
+  const handleRegister = async (): Promise<void> => {
+    const { onTokenChange } = props
 
     try {
-      const register = await session.register(
-        this.registerUrl,
-        username,
-        password
-      )
+      const register = await session.register(registerUrl, username, password)
       onTokenChange(register)
     } catch (e) {
       alert(e.message)
     }
   }
 
-  handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ username: e.target.value })
+  const handleUsernameChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setUsername(e.target.value)
   }
 
-  handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ password: e.target.value })
+  const handlePasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setPassword(e.target.value)
   }
 
-  handleKeyUp = (e: React.KeyboardEvent): void => {
+  const handleKeyUp = (e: React.KeyboardEvent): void => {
     if (e.key === 'Enter') {
-      this.handleLoginDebounced()
+      handleLoginDebounced()
     }
   }
 
-  render(): React.ReactNode {
-    const {
-      handleUsernameChange,
-      handlePasswordChange,
-      handleKeyUp,
-      handleLoginDebounced,
-      handleRegisterDebounced
-    } = this
-    const { username, password } = this.state
-    const disabled =
-      username.trim().length === 0 || password.trim().length === 0
+  const handleLoginDebounced = debounce(handleLogin, 200)
+  const handleRegisterDebounced = debounce(handleRegister, 200)
+  const disabled = username.trim().length === 0 || password.trim().length === 0
 
-    return (
-      <div>
-        <h1 className="header">Login or Register</h1>
-        <div className="login-container">
-          <input
-            value={username}
-            onChange={handleUsernameChange}
-            onKeyUp={handleKeyUp}
-            className="name-input"
-            type="text"
-            placeholder="Username"
-          />
-          <input
-            value={password}
-            onChange={handlePasswordChange}
-            onKeyUp={handleKeyUp}
-            className="pass-input"
-            type="password"
-            placeholder="Password"
-          />
-          <button
-            onClick={handleLoginDebounced}
-            className="login-button"
-            disabled={disabled}
-          >
-            Login
-          </button>
-          <button
-            onClick={handleRegisterDebounced}
-            className="register-button"
-            disabled={disabled}
-          >
-            Register
-          </button>
-        </div>
+  return (
+    <div>
+      <h1 className="header">Login or Register</h1>
+      <div className="login-container">
+        <input
+          value={username}
+          onChange={handleUsernameChange}
+          onKeyUp={handleKeyUp}
+          className="name-input"
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          value={password}
+          onChange={handlePasswordChange}
+          onKeyUp={handleKeyUp}
+          className="pass-input"
+          type="password"
+          placeholder="Password"
+        />
+        <button
+          onClick={handleLoginDebounced}
+          className="login-button"
+          disabled={disabled}
+        >
+          Login
+        </button>
+        <button
+          onClick={handleRegisterDebounced}
+          className="register-button"
+          disabled={disabled}
+        >
+          Register
+        </button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default LoginForm
